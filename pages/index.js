806ff9881a1a3d02a5e6f126b21b1a929b5fbdf4
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2022-09-27 17:27:03 +0800

    01_Sanity Applied with Banner

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+import { client } from '../lib/client';
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
@@ -12,12 +14,24 @@ const Home = () => {
       </div>
 
       <div className="products-container">
-        {['Product 1', 'Product 2'].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
     </>
-  )
+  );
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home;
